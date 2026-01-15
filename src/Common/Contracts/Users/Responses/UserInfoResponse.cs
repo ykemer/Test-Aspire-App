1e8dee94750622fa9e32b4d989ab3846526120c5
yakov.kemer <yakov.kemer@gmail.com>
2026-01-16 00:09:39 +0400

    Add UserInfoResponse class for user information retrieval

---
+++ src/Common/Contracts/Users/Responses/UserInfoResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Contracts.Users.Responses;
+
+public class UserInfoResponse
+{
+  public string Id { get; set; }
+  public string FirstName { get; set; }
+  public string LastName { get; set; }
+  public string Email { get; set; }
+}
